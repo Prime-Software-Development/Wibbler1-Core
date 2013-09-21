d40aedf9037fdf87da7ee3f854073e4af104d4ec
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2013-09-21 07:43:34 +0000

    More work on install base code

--- system/install/controllers/BaseController.php
+++ system/install/controllers/BaseController.php
@@ -16,12 +16,40 @@ class BaseController extends \Wibbler\WibblerController {
 	}
 
 	/**
+	 * Checks if the current user is logged in
+	 */
+	public function CheckUser()
+	{
+		$now = new \DateTime("now");
+
+		// If the user id doesn't exist or the session has expired
+		if (!isset($_SESSION['user_id']))
+		{
+			// Store the url the user has requested - once logged back in they can then be directed back
+			$_SESSION['calling_url'] =  $this->urls->request_uri;
+
+			// Redirect to the home page
+			$this->url->redirect('/');
+
+			// Stop running this code
+			exit();
+		}
+
+		return true;
+	}
+
+	/**
 	 * Generates the html to either render or display
 	 * @param type $template
 	 * @param type $data
 	 * @return type
 	 */
 	private function _GenerateTwig($template, $data) {
+		$data['system']['paths']['root'] = $this->urls->root_url;
+		$data['system']['paths']['resources'] = $this->urls->root_url . 'resources/';
+		$data['system']['paths']['css'] = $this->urls->root_url . 'resources/css/';
+		$data['system']['paths']['jscript'] = $this->urls->root_url . 'resources/js/';
+
 		return $this->twig->render($template, $data);
 	}
 
--- system/install/controllers/welcome.php
+++ system/install/controllers/welcome.php
@@ -6,13 +6,41 @@ $_ns = '\\MyApp';
 
 class Welcome extends \MyApp\BaseController {
 
+	$users = array(
+		array("id" => 1, "username" => "admin", "password" => "password")
+	);
+
 	function index() {
 
 		$action = $this->input->post('login_action');
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
 
 		switch ($action) {
 			case 'login':
-				//$_SESSION
+
+				foreach ($users as $user) {
+					if ($username == $user["username"] && $password = $user["password"]) {
+						$_SESSION["user_id"] = $user["id"];
+
+						// Set the default redirect path
+						$redirect_path = '/dashboard/';
+						// If there is a session variable with the calling url in it
+						if (isset($_SESSION['calling_url'])) {
+							// Change where to redirect to so the user gets to where they wanted
+							$redirect_path = '/' . $_SESSION['calling_url'];
+							// Clear the session variable
+							unset($_SESSION['calling_url']);
+						}
+
+						// Redirect to the home page or the calling url
+						$this->urls->redirect($redirect_path);
+						exit();
+					}
+				}
+				break;
+			case 'logout':
+				$_SESSION["user_id"] = null;
 				break;
 		}
 
