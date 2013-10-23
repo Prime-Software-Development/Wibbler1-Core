52bbaad409429b034ac1873b6fe3f4a97856852f
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2013-10-23 16:16:44 +0000

    Update to cache location

--- trunk/system/install/application/config/twig.php
+++ trunk/system/install/application/config/twig.php
@@ -2,4 +2,4 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['template_dir'] = APPPATH . 'templates';
-$config['cache_dir'] = BASEPATH . 'cache/twig';
+$config['cache_dir'] = BASEPATH . '/cache/twig';
