b65a7ee200aa654509d13654ae0e5fb8bfb40bcc
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2013-12-19 15:40:13 +0000

    Update to input module

--- system/modules/input.php
+++ system/modules/input.php
@@ -6,7 +6,10 @@ class input {
 	function __construct() {
 	}
 
-	function get($variable, $default = null) {
+	function get($variable = null, $default = null) {
+		if ($variable == null)
+			return $_GET;
+
 		if (isset($_GET[$variable]))
 			return $_GET[$variable];
 		else
