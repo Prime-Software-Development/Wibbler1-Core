edbf6c7613abe101447e64ce5aea09803fbb640c
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2014-03-03 14:28:43 +0000

    Branch of new version of Wibbler - ready for composer

---
+++ branches/new_version/wibbler_controller.php
@@ -0,0 +1,45 @@
+<?php
+namespace Trunk\Wibbler;
+require_once COMMONPATH . '/propel/generated-conf/config.php';
+
+class WibblerController {
+	private $_dependencies;
+	public $controller_path;
+
+	function __construct() {
+		$this->init( array(), null );
+	}
+
+	/**
+	 * Initiate the controller - called after construction by the main Wibbler class
+	 */
+	function init( $load_modules, $dependencies ) {
+
+		$this->_dependencies = $dependencies;
+
+		if ( $this->_dependencies !== null )
+			$this->_autoload = $this->_dependencies->getConfig('autoload');
+
+		foreach ( $load_modules as $module ) {
+			$module_path = "\\Trunk\\Wibbler\\Modules\\" . $module;
+			$this->$module = new $module_path;
+		}
+
+	}
+
+	/**
+	 * Load a user module - the file name and the class name must be identical
+	 * @param string $module Name of the module to load
+	 */
+	public function load_module($module, $namespace ) {
+		$this->$module = $this->_dependencies->getModule( $module, $namespace);
+	}
+
+	/**
+	 * Load a user helper file
+	 * @param string $helper Name of the helper file to load
+	 */
+	public function load_helper($helper) {
+		$this->_dependencies->getHelper($helper);
+	}
+}
