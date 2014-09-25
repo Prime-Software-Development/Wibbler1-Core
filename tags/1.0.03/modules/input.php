3aee6ba35e02ec4b38a9edcb04c4af75ebb9e74e
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2014-09-25 20:07:04 +0000

    Tagged v1.0.03

---
+++ tags/1.0.03/modules/input.php
@@ -0,0 +1,25 @@
+<?php
+namespace Wibbler\Modules;
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class input {
+	function __construct() {
+	}
+
+	function get($variable = null, $default = null) {
+		if ($variable == null)
+			return $_GET;
+
+		if (isset($_GET[$variable]))
+			return $_GET[$variable];
+		else
+			return $default;
+	}
+
+	function post($variable, $default = null) {
+		if (isset($_POST[$variable]))
+			return $_POST[$variable];
+		else
+			return $default;
+	}
+}
