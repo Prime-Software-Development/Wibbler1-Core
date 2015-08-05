12202c23a6729030dab7a0e8b1cc2d10c4fc83a2
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2015-08-05 16:15:35 +0000

    Update to provide single point of inheritance for split web controller directories Update to seperate loading js files into seperate twig file Update to provide includes for Bootstrap and jQuery UI

--- trunk/install/web1/index.php
+++ trunk/install/web1/index.php
@@ -1,4 +1,7 @@
 <?php
+// Change directory to current location - allows scripts to run via cron properly
+chdir( __dir__ );
+
 require '../vendor/autoload.php';
 
 // Name of the class to use as the main index for controllers
@@ -9,11 +12,52 @@ $index_class = 'welcome';
  */
 $index_method = 'index';
 
-// Path to the system folder
-define('BASEPATH', str_replace("\\", "/", __dir__));
-define('COMMONPATH', str_replace("\\", "/", __dir__) . '/../common/');
-define('CONTROLLERPATH', str_replace("\\", "/", __dir__ . '/../common/web1/'));
+// Options for environment are development or production
+define('ENVIRONMENT', 'development');
 
+// Definition of what this area is used for
+define( 'SYSTEMAREA', 'Admin' );
+
+// Switch on the current environment to set error reporting
+switch (ENVIRONMENT)
+{
+	case 'development':
+		error_reporting(E_ALL);
+		ini_set('display_errors', '1');
+		break;
+	case 'production':
+		error_reporting(0);
+
+		if (extension_loaded('newrelic')) {
+			newrelic_set_appname( "App Name" );
+		}
+		break;
+}
+
+/**
+ * Path to  /web1
+ */
+define( 'BASEPATH', str_replace("\\", "/", __dir__) );
+
+/**
+ * Path to the common file area
+ */
+define( 'COMMONPATH', str_replace("\\", "/", __dir__) . '/../common/' );
+
+/**
+ * Path to this directory's controllers
+ */
+define( 'CONTROLLERPATH', str_replace("\\", "/", __dir__ . '/../common/web1/') );
+
+/**
+ * File upload root path
+ */
+define('UPLOADPATH', str_replace("\\", "/", __dir__) . '/../common/uploads/');
+
+/**
+ * Temporary file upload path
+ */
+define('TEMPPATH', UPLOADPATH . 'tmp/');
 
 $load_modules = array('urls', 'input', 'twig');
 $load_helpers = array();
