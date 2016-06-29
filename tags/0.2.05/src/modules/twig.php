36da032085ef9a1869d767987981eafef018cc6e
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2016-06-29 14:28:48 +0000

    Tagged 0.2.05

---
+++ tags/0.2.05/src/modules/twig.php
@@ -0,0 +1,57 @@
+<?php
+namespace Trunk\Wibbler\Modules;
+
+class twig extends base {
+
+	private $_twig;
+	private $_template_dir;
+	private $_cache_dir;
+
+	function __construct( )
+	{
+		global $twig_options;
+		$this->_config = $twig_options;
+
+		$this->_template_dir = $this->_config['template_dir'];
+		$this->_cache_dir = $this->_config['cache_dir'];
+
+		#\Twig_Autoloader::register();
+
+		$loader = new \Twig_Loader_Filesystem($this->_template_dir);
+		$this->_twig = new \Twig_Environment($loader, array(
+			'cache' => $this->_cache_dir,
+			'debug' => true,
+		));
+		
+	}
+
+	public function render($template, $data = array()) {
+
+		$template = $this->_twig->loadTemplate($template);
+
+		return $template->render($data);
+	}
+	
+	public function display($template, $data = array()) {
+
+		$template = $this->_twig->loadTemplate($template);
+
+		$template->display($data);
+	}
+	/**
+	 * Adds a new filter function to the twig environment
+	 * @param type $name Name of the filter
+	 * @param type $filter Function to call
+	 */
+	public function add_filter($name, $filter) {
+		$this->_twig->addFilter($name, new \Twig_Filter_Function($filter));
+	}
+
+	public function set_number_format($decimal_places = 0, $decimal_point_char = ".", $thousand_seperator = ",") {
+		$this->_twig->getExtension('core')->setNumberFormat($decimal_places, $decimal_point_char, $thousand_seperator);
+	}
+
+    public function add_global( $name, $value ) {
+        $this->_twig->addGlobal( $name, $value );
+    }
+}
