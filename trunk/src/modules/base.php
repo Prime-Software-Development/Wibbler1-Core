d27d04423a8c2b5f29dfa0f3ec33ebdf1a7e95f1
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2017-07-14 14:18:05 +0000

    Addition of load_module and load_service to base module class

--- trunk/src/modules/base.php
+++ trunk/src/modules/base.php
@@ -11,6 +11,11 @@ abstract class base {
 
 	static $instances = array();
 
+	/**
+	 * Get an instance of this class
+	 * @param $options
+	 * @return mixed
+	 */
 	final public static function Instance( $options )
 	{
 		//static $instances = array();
@@ -25,4 +30,23 @@ abstract class base {
 		return self::$instances[$calledClass];
 	}
 
+	/**
+	 * Load the service
+	 * @param $service_name
+	 * @return bool
+	 */
+	public function load_service( $service_name ) {
+		return $this->dependencies->getService( $service_name );
+	}
+
+	/**
+	 * Load the module and return it
+	 * @param $module_name
+	 * @param null $namespace
+	 * @param null $options
+	 * @return mixed
+	 */
+	public function load_module( $module_name, $namespace = null, $options = null ) {
+		return $this->dependencies->getModule( $module_name, $namespace, $options );
+	}
 }
