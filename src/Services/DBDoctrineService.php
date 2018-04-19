dcfccfc2e7f0e59ec56bff4219018e43dadd3952
chris <chris@cba21473-6b85-4929-bd64-2f7380d6f324>
2018-04-19 16:34:48 +0000

    Improvements to doctrine handler

--- src/Services/DBDoctrineService.php
+++ src/Services/DBDoctrineService.php
@@ -18,9 +18,20 @@ class DBDoctrineService {
 			'user' => $additional_config[ 'user' ],
 			'password' => $additional_config[ 'password' ],
 			'dbname' => $additional_config[ 'dbname' ],
+			'host' => $additional_config[ 'host' ],
 		];
 		$config = Setup::createAnnotationMetadataConfiguration([$additional_config[ 'path_to_entities' ]], ENVIRONMENT != "production" );
 		$this->entity_manager = EntityManager::create( $dbParams, $config );
+
+		/*use Doctrine\Common\Annotations\AnnotationReader;
+		use Doctrine\Common\Annotations\AnnotationRegistry;
+
+			AnnotationRegistry::registerFile("/path/to/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php");
+			AnnotationRegistry::registerAutoloadNamespace("Symfony\Component\Validator\Constraint", "/path/to/symfony/src");
+			AnnotationRegistry::registerAutoloadNamespace("MyProject\Annotations", "/path/to/myproject/src");
+
+			$reader = new AnnotationReader();
+			AnnotationReader::addGlobalIgnoredName('dummy');*/
 	}
 
 	public function getEntityManager() {
